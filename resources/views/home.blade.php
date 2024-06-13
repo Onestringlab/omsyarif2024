e67d7b13d0bb9dbb39401d804ef9ac3af1a2a0d9
33221012 Yulrio Brianorman <33221012@std.stei.itb.ac.id>
2024-06-13 14:14:13 +0700

    Beranda Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 			<div class="card border-success ">
 				<div class="card-header text-bg-success ">ONLINE MONITORING OF SALARY AND REMUNERATION INFORMATION</div>
 				<div class="card-body">
-					<div>OM-SYARIF adalah aplikasi online internal satuan kerja yang berguna bagi pegawai untuk memonitor informasi gaji dan tunjangan dengan mudah dan aman, di mana saja dan kapan saja.</div>
+					<div>OM-SYARIF adalah aplikasi online internal satuan kerja yang berguna bagi pegawai untuk memonitor informasi gaji dan tunjangan dengan mudah dan aman, di mana saja dan kapan saja. Joss!</div>
 				</div>
 			</div>
 		</div>
