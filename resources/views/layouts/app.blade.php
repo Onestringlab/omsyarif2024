62cbc56aeefdd898e6ad1a437429bfd6453461db
33221012 Yulrio Brianorman <33221012@std.stei.itb.ac.id>
2024-06-25 10:01:14 +0700

    Modify column satker

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,9 @@
   <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
+  <!-- Custom CSS -->
+  <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+
   <!-- Scripts -->
   <!-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) -->
   <script src="{{ asset('build/assets/app.js') }}" defer></script>
