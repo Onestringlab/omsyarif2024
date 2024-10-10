2ada8df1fd78f84697c2c954feb25ec5d9665cf9
yulrio <33221012@std.stei.itb.ac.id>
2024-10-10 14:36:39 +0700

    Update Modul Satker

--- resources/views/satker/satkerform.blade.php
+++ resources/views/satker/satkerform.blade.php
@@ -10,124 +10,87 @@ Data Satker 
         function button_cancel() {
             location.replace("{{ asset('/') }}satker");
         }
-
     </script>
     <div class="card border-success">
         <h5 class="card-header text-bg-success"> Data Satuan Kerja</h5>
         <div class="card-body">
             @if($action == 'insert')
-                <form class="form-horizontal" action="{{ asset('/') }}satker" method="post">
-                    <div class="mb-3 row">
-                        <label for="kode" class="col-sm-2 col-form-label">Kode</label>
-                        <div class="col-sm-10">
-                            <input class="form-control" type="text" name="kode" value="" required>
-                        </div>
+            <form class="form-horizontal" action="{{ asset('/') }}satker" method="post">
+                <div class="mb-3 row">
+                    <label for="kode" class="col-sm-2 col-form-label">Kode</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" name="kode" value="" required>
                     </div>
-                    <div class="mb-3 row">
-                        <label for="nama" class="col-sm-2 col-form-label">Nama</label>
-                        <div class="col-sm-10">
-                            <input class="form-control" type="text" name="nama" value="" required>
-                        </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" name="nama" value="" required>
                     </div>
-                    <div class="mb-3 row">
-                        <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
-                        <div class="col-sm-10">
-                            <input class="form-control" type="text" name="alamat" value="" required>
-                        </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" name="alamat" value="" required>
                     </div>
-                    <div class="mb-3 row">
-                        <label for="keterangan" class="col-sm-2 col-form-label">Keterangan</label>
-                        <div class="col-sm-10">
-                            <input class="form-control" type="text" name="keterangan" value="" required>
-                        </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="keterangan" class="col-sm-2 col-form-label">Keterangan</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" name="keterangan" value="" required>
                     </div>
-                    <div class="mb-3">
-                        <div class="offset-sm-2 col-sm-10">
-                            <input type="hidden" name="action" value="{{ $action }}">
-                            <button type="submit" class="btn btn-primary">Insert</button>
-                            <button type="button" class="btn btn-secondary" onclick="button_cancel()">Kembali</button>
-                        </div>
+                </div>
+                <div class="mb-3">
+                    <div class="offset-sm-2 col-sm-10">
+                        <input type="hidden" name="action" value="{{ $action }}">
+                        <button type="submit" class="btn btn-primary">Tambah</button>
+                        <button type="button" class="btn btn-secondary" onclick="button_cancel()">Kembali</button>
                     </div>
-                    {{ csrf_field() }}
-                </form>
+                </div>
+                {{ csrf_field() }}
+            </form>
             @elseif($action == 'update')
-                <form class="form-horizontal" action="{{ asset('/') }}satker/{{ $row->id }}"
-                    method="post">
-                    <div class="mb-3 row">
-                        <label for="kode" class="col-sm-2 col-form-label">Kode</label>
-                        <div class="col-sm-10">
-                            <input class="form-control" type="text" name="kode" value="{{ $row->kode }}" required>
-                        </div>
+            <form class="form-horizontal" action="{{ asset('/') }}satker/{{ $row->id }}"
+                method="post">
+                <div class="mb-3 row">
+                    <label for="kode" class="col-sm-2 col-form-label">Kode</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" name="kode" value="{{ $row->kode }}" required>
                     </div>
-                    <div class="mb-3 row">
-                        <label for="nama" class="col-sm-2 col-form-label">Nama</label>
-                        <div class="col-sm-10">
-                            <input class="form-control" type="text" name="nama" value="{{ $row->nama }}" required>
-                        </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" name="nama" value="{{ $row->nama }}" required>
                     </div>
-                    <div class="mb-3 row">
-                        <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
-                        <div class="col-sm-10">
-                            <input class="form-control" type="text" name="alamat" value="{{ $row->alamat }}" required>
-                        </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" name="alamat" value="{{ $row->alamat }}" required>
                     </div>
-                    <div class="mb-3 row">
-                        <label for="keterangan" class="col-sm-2 col-form-label">Keterangan</label>
-                        <div class="col-sm-10">
-                            <input class="form-control" type="text" name="keterangan" value="{{ $row->keterangan }}"
-                                required>
-                        </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="keterangan" class="col-sm-2 col-form-label">Keterangan</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" name="keterangan" value="{{ $row->keterangan }}"
+                            required>
                     </div>
-                    <div class="mb-3 row">
-                        <div class="offset-sm-2 col-sm-10">
-                            @method("PATCH")
-                            <input type="hidden" name="action" value="{{ $action }}">
-                            <input type="hidden" name="id" value="{{ $row->id }}">
-                            <button type="submit" class="btn btn-warning">Update</button>
-                            <button type="button" class="btn btn-secondary" onclick="button_cancel()">Kembali</button>
-                        </div>
+                </div>
+                <div class="mb-3 row">
+                    <div class="offset-sm-2 col-sm-10">
+                        @method("PATCH")
+                        <input type="hidden" name="action" value="{{ $action }}">
+                        <input type="hidden" name="id" value="{{ $row->id }}">
+                        <button type="submit" class="btn btn-warning">Simpan</button>
+                        <button type="button" class="btn btn-secondary" onclick="button_cancel()">Kembali</button>
                     </div>
-                    {{ csrf_field() }}
-                </form>
+                </div>
+                {{ csrf_field() }}
+            </form>
             @elseif($action == 'delete')
-                <form class="form-horizontal" action="{{ asset('/') }}satker/{{ $row->id }}"
-                    method="post">
-                    <div class="mb-3 row">
-                        <label for="kode" class="col-sm-2 control-label">Kode</label>
-                        <div class="col-sm-10">
-                            {{ $row->kode }}
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="nama" class="col-sm-2 control-label">Nama</label>
-                        <div class="col-sm-10">
-                            {{ $row->nama }}
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="alamat" class="col-sm-2 control-label">Alamat</label>
-                        <div class="col-sm-10">
-                            {{ $row->alamat }}
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="keterangan" class="col-sm-2 control-label">Keterangan</label>
-                        <div class="col-sm-10">
-                            {{ $row->keterangan }}
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <div class="offset-sm-2 col-sm-10">
-                            @method("DELETE")
-                            <input type="hidden" name="action" value="{{ $action }}">
-                            <input type="hidden" name="id" value="{{ $row->id }}">
-                            <button type="submit" class="btn btn-danger">Hapus</button>
-                            <button type="button" class="btn btn-secondary" onclick="button_cancel()">Kembali</button>
-                        </div>
-                    </div>
-                    {{ csrf_field() }}
-                </form>
-            @elseif($action == 'detail')
+            <form class="form-horizontal" action="{{ asset('/') }}satker/{{ $row->id }}"
+                method="post">
                 <div class="mb-3 row">
                     <label for="kode" class="col-sm-2 control-label">Kode</label>
                     <div class="col-sm-10">
@@ -154,9 +117,45 @@ Data Satker 
                 </div>
                 <div class="mb-3 row">
                     <div class="offset-sm-2 col-sm-10">
-                        <button type="button" class="btn btn-secondary" onclick="button_cancel()">Back</button>
+                        @method("DELETE")
+                        <input type="hidden" name="action" value="{{ $action }}">
+                        <input type="hidden" name="id" value="{{ $row->id }}">
+                        <button type="submit" class="btn btn-danger">Hapus</button>
+                        <button type="button" class="btn btn-secondary" onclick="button_cancel()">Kembali</button>
                     </div>
                 </div>
+                {{ csrf_field() }}
+            </form>
+            @elseif($action == 'detail')
+            <div class="mb-3 row">
+                <label for="kode" class="col-sm-2 control-label">Kode</label>
+                <div class="col-sm-10">
+                    {{ $row->kode }}
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="nama" class="col-sm-2 control-label">Nama</label>
+                <div class="col-sm-10">
+                    {{ $row->nama }}
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="alamat" class="col-sm-2 control-label">Alamat</label>
+                <div class="col-sm-10">
+                    {{ $row->alamat }}
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="keterangan" class="col-sm-2 control-label">Keterangan</label>
+                <div class="col-sm-10">
+                    {{ $row->keterangan }}
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <div class="offset-sm-2 col-sm-10">
+                    <button type="button" class="btn btn-secondary" onclick="button_cancel()">Back</button>
+                </div>
+            </div>
             @endif
         </div>
     </div>
--- resources/views/satker/satkerlist.blade.php
+++ resources/views/satker/satkerlist.blade.php
@@ -13,13 +13,13 @@ Data Satker 
                 <table class="table table-striped table-hover ">
                     <thead class="thead-dark">
                         <tr>
-                            <th class="text-center">No</th>
-                            <th class="text-center">Kode</th>
-                            <th class="text-center">Nama</th>
+                            <th class="text-center" width="50px">No</th>
+                            <th class="text-center" width="100px">Kode</th>
+                            <th class="text-center" width="275px">Nama</th>
                             <th class="text-center">Alamat</th>
                             <!-- <th class="text-center">Created_at</th> -->
-                            <th class="text-center">Updated at</th>
-                            <th class="text-center"><a class="btn btn-primary" href="{{ asset('/') }}satker/create">
+                            <th class="text-center" width="180px">Waktu</th>
+                            <th class="text-center" width="180px"><a class=" btn btn-primary" href="{{ asset('/') }}satker/create">
                                     <i class="fas fa-plus"></i>
                                 </a>
                             </th>
@@ -27,24 +27,24 @@ Data Satker 
                     </thead>
                     <tbody>
                         @php($no = 1)
-                            @foreach($rows as $row)
-                                <tr>
-                                    <td>{{ $no++ }}.</td>
-                                    <td>{{ $row['kode'] }}</td>
-                                    <td>{{ $row['nama'] }}</td>
-                                    <td>{{ $row['alamat'] }}</td>
-                                    <!-- <td>{{ $row['created_at'] }}</td> -->
-                                    <td>{{ $row['updated_at'] }}</td>
-                                    <td align="center">
-                                        <a class="btn btn-success" href="{{ asset('/') }}satker/{{ $row->id }}"><i
-                                                class="fas fa-info-circle"></i></a>
-                                        <a class="btn btn-secondary" href="{{ asset('/') }}satker/{{ $row->id }}/edit"><i
-                                                class="far fa-edit"></i></a>
-                                        <a class="btn btn-danger" href="{{ asset('/') }}satker/{{ $row->id }}/delete"><i
-                                                class="far fa-trash-alt"></i></a>
-                                    </td>
-                                </tr>
-                            @endforeach
+                        @foreach($rows as $row)
+                        <tr>
+                            <td class="text-center">{{ $no++ }}.</td>
+                            <td>{{ $row['kode'] }}</td>
+                            <td>{{ $row['nama'] }}</td>
+                            <td>{{ $row['alamat'] }}</td>
+                            <!-- <td>{{ $row['created_at'] }}</td> -->
+                            <td>{{ $row['updated_at'] }}</td>
+                            <td align="center">
+                                <a class="btn btn-success" href="{{ asset('/') }}satker/{{ $row->id }}"><i
+                                        class="fas fa-info-circle"></i></a>
+                                <a class="btn btn-secondary" href="{{ asset('/') }}satker/{{ $row->id }}/edit"><i
+                                        class="far fa-edit"></i></a>
+                                <a class="btn btn-danger" href="{{ asset('/') }}satker/{{ $row->id }}/delete"><i
+                                        class="far fa-trash-alt"></i></a>
+                            </td>
+                        </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
