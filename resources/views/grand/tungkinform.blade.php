5d92731759e7ad62f84b4f4c81713fb41ca7c002
Onestringlab <onestringlab@gmail.com>
2025-04-22 10:34:20 +0700

    Update tungkin form "perhitungan"

--- resources/views/grand/tungkinform.blade.php
+++ resources/views/grand/tungkinform.blade.php
@@ -7,22 +7,22 @@ Tunjangan Kinerja
 @section('content')
 <div class="container">
     <script>
-    function button_cancel() {
-        location.replace("{{ asset('/') }}tungkinlist");
-    }
+        function button_cancel() {
+            location.replace("{{ asset('/') }}tungkinlist");
+        }
     </script>
     <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
         <ol class="breadcrumb">
-        <li class="breadcrumb-item">
-            <a href="{{ asset('/tungkinlist') }}">Tunjangan Kinerja</a>
-        </li>
-        <li class="breadcrumb-item active" aria-current="page">
-            {{ $row->months->month }} {{ $row->months->year }}
-        </li>
+            <li class="breadcrumb-item">
+                <a href="{{ asset('/tungkinlist') }}">Tunjangan Kinerja</a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">
+                {{ $row->months->month }} {{ $row->months->year }}
+            </li>
         </ol>
     </nav>
     <div class="card border-success">
-        <h5 class="card-header text-bg-success"> Tunjangan Kinerja
+        <h5 class="card-header text-bg-success"> Perhitungan Tunjangan Kinerja
             {{ $row->months->month }} {{ $row->months->year }}
         </h5>
         <div class="card-body">
@@ -30,28 +30,28 @@ Tunjangan Kinerja
                 <div class="mb-3 row">
                     <label for="status" class="col-sm-2 col-form-label"><strong>Status</strong></label>
                     <div class="col-sm-10">
-                    <select class="form-control" type="text" name="status">
-                        <option value="Setuju" {{ $row->status === "Setuju" ? "selected" : ""}}>Setuju</option>
-                        <option value="Tidak Setuju" {{ $row->status === "Tidak Setuju" ? "selected" : ""}}>Tidak Setuju</option>
-                    </select>
+                        <select class="form-control" type="text" name="status">
+                            <option value="Setuju" {{ $row->status === "Setuju" ? "selected" : ""}}>Setuju</option>
+                            <option value="Tidak Setuju" {{ $row->status === "Tidak Setuju" ? "selected" : ""}}>Tidak Setuju</option>
+                        </select>
                     </div>
                 </div>
                 <div class="mb-3 row">
                     <label for="alasan" class="col-sm-2 col-form-label"><strong>Alasan</strong></label>
                     <div class="col-sm-10">
-                    <input class="form-control" type="text" name="alasan" value="{{ $row->alasan }}">
+                        <input class="form-control" type="text" name="alasan" value="{{ $row->alasan }}">
                     </div>
                 </div>
                 <div class="mb-3 row">
                     <div class="offset-sm-2 col-sm-10">
-                    <input type="hidden" name="id" value="{{ $row->id }}">
-                    <input type="hidden" name="month_id" value="{{ $row->month_id }}">
-                    <button type="submit" class="btn btn-warning">Simpan</button>
-                    <button type="button" class="btn btn-secondary" onclick="button_cancel()">Kembali</button>
+                        <input type="hidden" name="id" value="{{ $row->id }}">
+                        <input type="hidden" name="month_id" value="{{ $row->month_id }}">
+                        <button type="submit" class="btn btn-warning">Simpan</button>
+                        <button type="button" class="btn btn-secondary" onclick="button_cancel()">Kembali</button>
                     </div>
                 </div>
                 {{ csrf_field() }}
-        </form>
+            </form>
         </div>
     </div>
 </div>
