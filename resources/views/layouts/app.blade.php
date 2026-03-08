fbd7bc7705f002d8d9da521773f2c11a9ae46c92
Onestringlab <onestringlab@gmail.com>
2026-03-08 08:07:42 +0700

    update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
 <link rel="dns-prefetch" href="//fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-<link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
+<!-- <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}"> -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
